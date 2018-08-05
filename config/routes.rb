5bfc3aca49047d2acc46eb7fb41ce2c5f84bac24
Tom Quant <tom@Toms-MacBook-Air.local>
2018-08-06 09:43:15 +1000

    events and groups controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+
+
+  get 'group/index'
+  get 'group/show'
+  get 'group/new'
+  get 'group/edit'
+resources :events
+resources :groups
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
