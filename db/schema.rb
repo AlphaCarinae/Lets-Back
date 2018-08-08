47e391ffd51720ec2bc3ed83e1dcac9c68a247bb
Soheil <Soheil.Tajahmadi@gmail.com>
2018-08-08 12:05:13 +1000

    before branching for JWT login

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_07_050716) do
+ActiveRecord::Schema.define(version: 2018_08_07_055543) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -69,8 +69,8 @@ ActiveRecord::Schema.define(version: 2018_08_07_050716) do
   create_table "roles", force: :cascade do |t|
     t.integer "user_id"
     t.integer "group_id"
-    t.boolean "admin", default: false
-    t.boolean "moderator", default: false
+    t.boolean "admin"
+    t.boolean "moderator"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
